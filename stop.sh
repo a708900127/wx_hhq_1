5f7bb393cda401474fd53fc089ac19da8e0ab5d2
Administrator <Administrator@BF-20190301WTQQ>
2019-07-04 21:12:04 +0800

    最后一次提交111

--- stop.sh
+++ stop.sh
@@ -10,8 +10,9 @@ mvn spring-boot:stop
 cd ../unsubscribe
 mvn spring-boot:stop
 
-cd ../self_menu
-mvn spring-boot:stop
 
 cd ../library
+mvn spring-boot:stop
+
+cd ../self_menu
 mvn spring-boot:stop
